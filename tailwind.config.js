60f23dde0aee0127d20e9ac3265806ba3c7c852f
Aurélien Guelle <aurelien.guelle@gmail.com>
2024-07-19 17:45:51 +0200

    start fbab project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custombrown: '#d09c61',
+        customblue: '#132533',
+      },
+    },
+  },
+  plugins: [],
+}
