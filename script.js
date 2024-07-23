b82ca3169fbc9f264f018393ab545bf58c95d732
Aurélien Guelle <aurelien.guelle@gmail.com>
2024-07-23 11:21:43 +0200

    modify tailwind config

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// JavaScript pour gérer l'ouverture et la fermeture du menu mobile
+// Oouverture et fermeture du menu mobile
 document.getElementById('menuButton').addEventListener('click', function () {
-    var menu = document.getElementById('mobileMenu');
-    var openIcon = document.getElementById('menuOpenIcon');
-    var closeIcon = document.getElementById('menuCloseIcon');
+    let menu = document.getElementById('mobileMenu');
+    let openIcon = document.getElementById('menuOpenIcon');
+    let closeIcon = document.getElementById('menuCloseIcon');
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         menu.classList.add('block');
@@ -15,7 +15,7 @@ document.getElementById('menuButton').addEventListener('click', function () {
         closeIcon.classList.add('hidden');
     }
 });
-// JavaScript pour gérer l'affichage des contenus des services
+// Affichage des contenus des services
 document.getElementById('btnConstruction').addEventListener('click', function () {
     showContent('construction');
 });
@@ -27,16 +27,16 @@ document.getElementById('btnAmenagement').addEventListener('click', function () 
 });
 
 function showContent(type) {
-    var contents = ['construction', 'renovation', 'amenagement'];
+    let contents = ['construction', 'renovation', 'amenagement'];
     contents.forEach(content => {
         if (content === type) {
             document.getElementById(`content${capitalize(content)}`).classList.remove('hidden');
             document.getElementById(`content${capitalize(content)}`).classList.add('block');
-            document.getElementById(`btn${capitalize(content)}`).classList.add('bg-custombrown', 'text-white');
+            document.getElementById(`btn${capitalize(content)}`).classList.add('bg-twine', 'text-white');
         } else {
             document.getElementById(`content${capitalize(content)}`).classList.add('hidden');
             document.getElementById(`content${capitalize(content)}`).classList.remove('block');
-            document.getElementById(`btn${capitalize(content)}`).classList.remove('bg-custombrown', 'text-white');
+            document.getElementById(`btn${capitalize(content)}`).classList.remove('bg-twine', 'text-white');
         }
     });
 }
@@ -47,7 +47,7 @@ function capitalize(word) {
 
 // Initial display
 showContent('construction');
-// JavaScript pour gérer la redirection vers les sections
+// Redirection vers les sections
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('a[href^="#"]');
     links.forEach(link => {
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// JavaScript pour gérer l'affichage des mentions légales
+//Affichage des mentions légales
 document.getElementById('toggleLegal').addEventListener('click', function (event) {
     event.preventDefault();
     var legalSection = document.getElementById('legalSection');
